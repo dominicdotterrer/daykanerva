d7a735fc7b74d9a52beec9d2709f85b32343cd30
Dominic Dotterrer <d.dotterrer@gmail.com>
2026-03-27 10:40:04 -0400

    Modified header and added first math game

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dominic Day Kanerva';
-export const SITE_DESCRIPTION = 'Personal site of Dominic Day Kanerva — research, math, and writing.';
+export const SITE_TITLE = "Dominic's Page";
+export const SITE_DESCRIPTION = 'Personal site of Dominic Dotterrer — research, math, and writing.';
